b91a00b45e4406372fcbc09d01bd26116c14b5fc
OpheNg <ophe.ng@live.fr>
2022-11-23 14:28:59 +0100

    routes ok, il faut faire le front de la navbar, et de standard premium & dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   get 'home', to: 'pages#home'
   get 'dashboard', to: "pages#dashboard"
+  get 'standard', to: "pages#standard"
+  get 'premium', to: "pages#premium"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
